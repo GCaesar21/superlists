7a27c3878b5490e7a9bac7c9e36437e7b5c87654
Yiyi <Yiyi@test.com>
2026-01-06 20:19:01 +0800

    Add first functional test with unittest

--- functional_tests.py
+++ functional_tests.py
@@ -1,14 +1,27 @@
 from selenium import webdriver
 from selenium.webdriver.firefox.service import Service
 from selenium.webdriver.firefox.options import Options
+import unittest
 
-options = Options()
-options.binary_location = r"D:\Program Files\Mozilla Firefox/firefox.exe"
-service = Service(executable_path=r"D:/Python/Scripts/geckodriver.exe")
+class NewVisitorTest(unittest.TestCase):   
 
 
-browser = webdriver.Firefox(service=service, options=options)
-browser.get('http://localhost:8000')
-browser.implicitly_wait(10)
+	def setUp(self):
+		options = Options()
+		options.binary_location = r"D:\Program Files\Mozilla Firefox/firefox.exe"
+		service = Service(executable_path=r"D:/Python/Scripts/geckodriver.exe")
+		self.browser = webdriver.Firefox(service=service, options=options)
 
-assert 'successfully' in browser.title
+	def tearDown(self):
+		self.browser.quit()
+	
+	def test_can_start_a_list_and_retrieve_it_later(self):
+		self.browser.get('http://localhost:8000')
+		self.browser.implicitly_wait(10)
+		self.assertIn('To-do',self.browser.title)
+		self.fail('Finish the test!')
+
+
+if __name__=='__main__':
+	unittest.main(warnings='ignore')
+#assert 'successfully' in browser.title, "Browser title was " +browser.title
