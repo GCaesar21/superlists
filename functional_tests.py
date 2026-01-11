a888b6a73083415e3903912af7935654576c4e9f
Yiyi <Yiyi@test.com>
2026-01-11 15:33:31 +0800

    Added a function to check if the specified text is in a row in a list table.Minor refactoring of the functional tests

--- functional_tests.py
+++ functional_tests.py
@@ -16,6 +16,11 @@ class NewVisitorTest(unittest.TestCase):
 
 	def tearDown(self):
 		self.browser.quit()
+
+	def check_for_row_in_list_table(self,row_text):
+		table=self.browser.find_element(by='id',value='id_list_table')
+		rows=table.find_elements(by='tag name',value='tr')
+		self.assertIn(row_text,[row.text for row in rows])
 	
 	def test_can_start_a_list_and_retrieve_it_later(self):
 		self.browser.get('http://localhost:8000')
@@ -44,10 +49,7 @@ class NewVisitorTest(unittest.TestCase):
 		inputbox.send_keys('Buy peacock feathers')
 		inputbox.send_keys(Keys.ENTER)
 		time.sleep(1)
-		
-		table=self.browser.find_element(by='id',value='id_list_table')
-		rows=table.find_elements(by='tag name',value='tr')
-		self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+		self.check_for_row_in_list_table('1: Buy peacock feathers')
 
 		#Enter the second to-do item
 		inputbox=self.browser.find_element(by='id',value='id_new_item')
@@ -55,10 +57,8 @@ class NewVisitorTest(unittest.TestCase):
 		inputbox.send_keys(Keys.ENTER)
 		time.sleep(1)
 
-		table=self.browser.find_element(by='id',value='id_list_table')
-		rows=table.find_elements(by='tag name',value='tr')
-		self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
-		self.assertIn('2: Use peacock feathers to make fly', [row.text for row in rows])		
+		self.check_for_row_in_list_table('1: Buy peacock feathers')
+		self.check_for_row_in_list_table('2: Use peacock feathers to make fly')
 
 		#A text box is displayed where you can enter additional items
 		self.fail('Finish the test!')
